93c9b074d67975fb1c73d12d3b919235b1e2fe72
KLChakraborty <chakrabortykajal2@gmail.com>
2022-12-27 22:00:24 +0530

    done

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -30,7 +30,7 @@ const createOrder = async function (req, res) {
 
         let createOrder = await orderModel.create(obj)
         await cartModel.findByIdAndUpdate({ _id: presentCart._id }, { $set: { items: [], totalPrice: 0, totalItems: 0 } })
-        return res.status(200).send({ status: false, message: 'Success', data: createOrder })
+        return res.status(201).send({ status: false, message: 'Success', data: createOrder })
     } catch (error) {
         return res.status(500).send({ status: false, message: error.message })
     }
@@ -63,7 +63,7 @@ const updateOrder = async function (req, res) {
             let updateOrder = await orderModel.findByIdAndUpdate({ _id: orderId }, { $set: { status: req.body.status } }, { new: true })
             return res.status(200).send({ status: true, message: 'Success', data: updateOrder })
         } else {
-            return res.status(404).send({ status: false, message: 'No order found' })
+            return res.status(404).send({ status: false, message: 'This order has been completed or cancelled' })
         }
     } catch (error) {
         return res.status(500).send({ status: false, message: error.message })
